0cb304700586647c48f39b393d3ca373435785a1
Aerol <rectifier04@gmail.com>
2012-03-18 00:03:21 -0500

    Added command hook
    Signed-off-by: Aerol <rectifier04@gmail.com>

--- wr.py
+++ wr.py
@@ -5,9 +5,21 @@ server = weechat.buffer_get_string(weechat.current_buffer(), 'localvar_server')
 channel = weechat.buffer_get_string(weechat.current_buffer(), 'localvar_channel')
 infolist = weechat.infolist_get('irc_nick', '', server + ',' + channel) 
 nicklist = ''
-while weechat.infolist_next(infolist):
-   nicklist += weechat.infolist_string(infolist, 'name')
-   nicklist += ' '
+#while weechat.infolist_next(infolist):
+#   nicklist += weechat.infolist_string(infolist, 'name')
+#   nicklist += ' '
+
+def wake_room(data, buffer, args):
+   while weechat.infolist_next(infolist):
+      nicklist += weechat.infolist_string(infolist, 'name')
+      nicklist += ' '
+   return weechat.WEECHAT_RC_OK
+
+# hook prototype
+#hook = weechat.hook_command(command, description, args, args_description,
+#completion, callback, callback_data)
+hook = weechat.hook_command("wakeroom", "Brings a dead IRC channel back to life!","","","","wake_room","")
+
 weechat.command(weechat.current_buffer(), nicklist)
 weechat.infolist_free(infolist)
 
