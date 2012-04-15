4729bdd7fa81f45ba56ff0855ff137688710edb0
BullShark <goodbye300@aim.com>
2012-04-15 00:54:21 -0500

    Commando no work, why not?

--- wr.py
+++ wr.py
@@ -10,8 +10,8 @@ def wake_room():
   while weechat.infolist_next(infolist):
     nicklist += weechat.infolist_string(infolist, 'name')
     nicklist += ' '
-
   weechat.command(weechat.current_buffer(), nicklist)
   weechat.infolist_free(infolist)
   return weechat.WEECHAT_RC_OK
+
 hook = weechat.hook_command("wakeroom", "Brings a dead IRC channel back to life!","","","","wake_room","")
